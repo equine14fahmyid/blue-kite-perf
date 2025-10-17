d218c4cd5e2cef29b920e440067597d224273e3d
equine14fahmyid <devfahmyid@gmail.com>
2025-10-17 16:05:37 +0700

    Import AddProductForm component in Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -15,7 +15,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { AddProductForm } from "@/components/AddProductForm";
+import { AddProductForm } from "@/components/AddProductForm"; // <-- Impor komponen baru
 
 // Tipe data untuk Product
 type Product = {
